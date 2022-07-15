2602e82283714854a86e91bef4a9b17d1d267f9e
maxamed <98mohamed.mo@gmail.com>
2022-07-15 22:00:24 +0100

    json text changed to v2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "this was just a test" });
+  res.json({ message: "this was just a test v2" });
 });
 
 const PORT = process.env.PORT || 8000;
